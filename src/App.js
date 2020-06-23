449ce6b07f9ff92d7b6db26829c2792e1ec6949a
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-06-24 00:00:29 +0200

    update api and function in app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Titles from './components/Titles.js'
 import Form from './components/Form.js'
 import Weather from './components/Weather.js'
 // API is made in openweathermap.org page
-const API_EKY = 'dc99351dfcb175a3f1977a687c118bdd'; 
+const API_KEY = 'dc99351dfcb175a3f1977a687c118bdd'; 
 class App extends React.Component{
+  // to stop reload or refreshing the page we give (e) arrangement and set is to prevent default act
+
  // async way is a way to call for http call
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     // await is calling for matted 
     // fetch is new way to call API from server
-    // in tetch we use templete string (``), it allow you to inject the variable in your file what you define
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+    // in fetch we use template string (``), it allow you to inject the variable in your file what you define
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Kabul,af&APPID=${API_KEY}`);
     // date await will get all the date from api_call and using json to convert it to readable format for use
     const date = await api_call.json();
     console.log(date);
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,8 @@ import React from 'react';
 class Form extends React.Component{
   render(){
     return(
-      <form>
+      // here we can access our prop by passing function in form
+      <form onSubmit = {this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
           <button>Get Weather!</button>
