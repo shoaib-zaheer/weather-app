843e8fcb33c082e1de38a352adf674306a1a0fdb
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-06-23 22:59:06 +0200

    update weather.js from weather branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Titles from './components/Titles'
-import Form from './components/Form'
+import Titles from './components/Titles.js'
+import Form from './components/Form.js'
+import Weather from './components/Weather.js'
 
 class App extends React.Component{
   render(){
@@ -9,6 +10,7 @@ class App extends React.Component{
 
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
