7dd43e2d3a88a1214dd252b4ebc106e32f38dfa4
shoaib <z.shoaib015@gmail.com>
2020-07-04 12:43:58 +0200

    update two page form and titles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
-class Form extends React.Component{
-  render(){
-    return(
-      // here we can access our prop by passing function in form
-      <form onSubmit = {this.props.getWeather}>
-        <input type="text" name="city" placeholder="City..." />
-        <input type="text" name="country" placeholder="Country..." />
+const Form = props =>(
+
+      <form onSubmit = {props.getWeather}>
+          <input type="text" name="city" placeholder="City..." />
+          <input type="text" name="country" placeholder="Country..." />
           <button>Get Weather!</button>
       </form>
-    );
-  }
-}
+);
+
+
+// class Form extends React.Component{
+//   render(){
+//     return(
+//       // here we can access our prop by passing function in form
+//       <form onSubmit = {this.props.getWeather}>
+//         <input type="text" name="city" placeholder="City..." />
+//         <input type="text" name="country" placeholder="Country..." />
+//           <button>Get Weather!</button>
+//       </form>
+//     );
+//   }
+// }
 export default Form;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,13 +1,23 @@
 import React from "react"
 
-class Titles extends React.Component{
-  render(){
-    return(
+
+
+const Titles = () => (
       <div>
         <h1>Weather finder</h1>
         <p>Find out temperature, conditions and more...</p>
       </div>
-    );
-  }
-}
+
+);
+
+// class Titles extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <h1>Weather finder</h1>
+//         <p>Find out temperature, conditions and more...</p>
+//       </div>
+//     );
+//   }
+// }
 export default Titles;
