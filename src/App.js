c17e611a8c1382721c2515190ee9c5d3be55cb5e
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-07-04 11:28:40 +0200

    update weather page

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ class App extends React.Component{
     this.setState({
       temperature: data.main.temp,
       city: data.name,
-      country:data.sys.country,
-      humidity:data.main.humidity,
-      description:data.weather[0].description,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
       error:""
     });
 
